bd7c9f587cd2c596f5a9c3fdc383ed8587f4d753
yeonwoo13 <kangyw2942@gmail.com>
2022-01-12 10:53:49 +0900

    commit

--- await-async.dart
+++ await-async.dart
@@ -1,15 +1,23 @@
-void main() async {
-  String data = await getDataFromServer(); //Future는 원래 미리 줘야하는데, 기다리게 만드는게 await이다. data가 왔음.
+Future<void> main() async{
+  //1.await를 쓰는 함수는 async 함수여야 한다.
+  //2.그 함수는 Future를 리턴해야 한다.
+
+  String data = await getDataFromServer();
   print(data);
 
-  print('a');
-  print('b');
-  print('c');
-  print('d');
+  print("a");
+  print("b");
+  print("c");
 }
 
-Future<String> getDataFromServer() async {
-  return Future.delayed(Duration(seconds: 1), () {
+Future<String> getDataFromServer() {
+  //가짜로 만들어본 Future 예제.
+  //Future.delayed 이름있는 생성자.
+  //delayed 생성자를 쓰면,
+  //delayed의 1번쨰 인자(Duration), 2번쨰 인자(함수)
+  //Duration 시간의양을 나타낼때 씀.. 3초.를 나타냄.
+  //2번째 인자의 함수는 Duration뒤에 실행되는것... 그래서, 그 함수의 결과를 then을 통해서 알려줌.
+  return Future.delayed(Duration(days: 3), () {
     return "data";
   });
 }
--- future.dart
+++ future.dart
@@ -1,18 +1,21 @@
 void main() {
   Future dataFuture = getDataFromServer();
-  //getDataFromServer를 이용해서 데이터를 받아서 A기능해라(print)해라.
   dataFuture.then((value){
     print(value);
   });
 
-  //저게 끝나기 전에, B기능, C기능, D기능을 순차적으로 실행해라.(pint)
-  print('a');
-  print('b');
-  print('c');
-  print('d');
+  print("a");
+  print("b");
+  print("c");
 }
 
 Future<String> getDataFromServer() {
+  //가짜로 만들어본 Future 예제.
+  //Future.delayed 이름있는 생성자.
+  //delayed 생성자를 쓰면,
+  //delayed의 1번쨰 인자(Duration), 2번쨰 인자(함수)
+  //Duration 시간의양을 나타낼때 씀.. 3초.를 나타냄.
+  //2번째 인자의 함수는 Duration뒤에 실행되는것... 그래서, 그 함수의 결과를 then을 통해서 알려줌.
   return Future.delayed(Duration(seconds: 3), () {
     return "data";
   });
